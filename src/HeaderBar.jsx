47277b13524dc41cfe00034ebf2bb44525cd487e
Paul Daniel <paulrdaniel1@gmail.com>
2026-04-12 19:17:42 +0100

    Added version number to header - left aligned

--- src/HeaderBar.jsx
+++ src/HeaderBar.jsx
@@ -4,22 +4,21 @@ function HeaderBar({ theme, toggleTheme }) {
   const version = packageJson.version;
 
   return (
-    <div 
-      className="header-bar" 
-      style={{ 
-        display: "flex", 
-        justifyContent: "space-between", 
-        alignItems: "flex-start" 
-      }}
-    >
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <h1 style={{ margin: 0, lineHeight: "1.2" }}>Contact Manager</h1>
-        <span 
-          style={{ 
-            fontSize: "0.75rem", 
-            color: "#888", 
+    <div className="header-bar">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+        }}
+      >
+        <h1 style={{ margin: 0, textAlign: "left" }}>Contact Manager</h1>
+        <span
+          style={{
+            fontSize: "0.75rem",
+            color: "#888",
             marginTop: "2px",
-            lineHeight: "1"
+            textAlign: "left",
           }}
         >
           v{version}
@@ -34,5 +33,6 @@ function HeaderBar({ theme, toggleTheme }) {
 }
 
 export default HeaderBar;
+
 
 
